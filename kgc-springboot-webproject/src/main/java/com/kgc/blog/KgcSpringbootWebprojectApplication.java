f6f420da4831c7a7f06d79061ee40012ea11d3f8
gogokgc <gogokgc@gmail.com>
2021-05-20 20:53:09 +0900

    first commit

---
+++ kgc-springboot-webproject/src/main/java/com/kgc/blog/KgcSpringbootWebprojectApplication.java
@@ -0,0 +1,13 @@
+package com.kgc.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KgcSpringbootWebprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KgcSpringbootWebprojectApplication.class, args);
+	}
+
+}
