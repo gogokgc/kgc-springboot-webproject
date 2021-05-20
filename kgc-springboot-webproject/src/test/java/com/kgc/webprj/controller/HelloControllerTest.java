79959451fdd463bde16ba2fb91f6fa1aa78d81ef
gogokgc <gogokgc@gmail.com>
2021-05-20 22:45:37 +0900

    rename

--- kgc-springboot-webproject/src/test/java/com/kgc/webprj/controller/HelloControllerTest.java
+++ kgc-springboot-webproject/src/test/java/com/kgc/webprj/controller/HelloControllerTest.java
@@ -9,8 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(controllers = HelloControllerTestTest.class)
-public class HelloControllerTestTest {
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
 
 	@Autowired
 	private MockMvc mvc;
