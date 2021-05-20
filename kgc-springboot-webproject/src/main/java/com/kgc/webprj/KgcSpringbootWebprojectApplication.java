c07d86f29bb88eafc98688089be4fd8df8b73eb7
gogokgc <gogokgc@gmail.com>
2021-05-20 21:07:39 +0900

    edit pakage name

--- kgc-springboot-webproject/src/main/java/com/kgc/webprj/KgcSpringbootWebprojectApplication.java
+++ kgc-springboot-webproject/src/main/java/com/kgc/webprj/KgcSpringbootWebprojectApplication.java
@@ -1,4 +1,4 @@
-package com.kgc.blog;
+package com.kgc.webprj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- kgc-springboot-webproject/src/test/java/com/kgc/webprj/KgcSpringbootWebprojectApplicationTests.java
+++ kgc-springboot-webproject/src/test/java/com/kgc/webprj/KgcSpringbootWebprojectApplicationTests.java
@@ -1,4 +1,4 @@
-package com.kgc.blog;
+package com.kgc.webprj;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
