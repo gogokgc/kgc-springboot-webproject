c8deeedc8e3829e8577c2a7d9c7be28dd91008c4
gogokgc <gogokgc@gmail.com>
2021-05-20 22:22:20 +0900

    테스트용 컨트롤러 작성, mysql 연동

---
+++ kgc-springboot-webproject/src/main/java/com/kgc/webprj/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.kgc.webprj.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/test/hello")
+	public String hello() {
+		
+		return "hello2";
+	}
+	
+}
---
+++ kgc-springboot-webproject/src/test/java/com/kgc/webprj/controller/HelloControllerTest.java
@@ -0,0 +1,26 @@
+package com.kgc.webprj.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+
+	@Autowired
+	private MockMvc mvc;
+	
+	@Test
+	public void returnHello() throws Exception{
+		String hello = "hello";
+		
+		mvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string(hello));
+		
+	}
+	
+}
