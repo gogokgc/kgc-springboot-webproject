fd9f5357a66b9d8726c3ab6ff096a72dfaea9b79
gogokgc <gogokgc@gmail.com>
2021-05-21 22:59:12 +0900

    Prepare Service Class

---
+++ kgc-springboot-webproject/src/main/java/com/kgc/webprj/service/PostService.java
@@ -0,0 +1,8 @@
+package com.kgc.webprj.service;
+
+import org.springframework.stereotype.Service;
+
+public class PostService {
+
+	
+}
---
+++ kgc-springboot-webproject/src/main/java/com/kgc/webprj/service/UserService.java
@@ -0,0 +1,5 @@
+package com.kgc.webprj.service;
+
+public class UserService {
+
+}
