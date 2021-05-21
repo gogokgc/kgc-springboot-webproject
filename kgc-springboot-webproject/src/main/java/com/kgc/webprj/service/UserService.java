b7830a459334f921dd2253096f9b4051d83d288d
gogokgc <gogokgc@gmail.com>
2021-05-22 00:09:57 +0900

    Edit Views, prepare Service Class

--- kgc-springboot-webproject/src/main/java/com/kgc/webprj/service/UserService.java
+++ kgc-springboot-webproject/src/main/java/com/kgc/webprj/service/UserService.java
@@ -1,5 +1,24 @@
 package com.kgc.webprj.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.kgc.webprj.model.RoleType;
+import com.kgc.webprj.model.User;
+import com.kgc.webprj.repository.UserRepository;
+
+@Service
 public class UserService {
 
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Transactional
+	public void joinMember(User user) {
+		
+		user.setRole(RoleType.USER);
+		userRepository.save(user);
+	}
+	
 }
